64fd9bf7fa58ea3d79faa405d756ff7da33faa5a
Clement-pirelli <clepirelli@gmail.com>
2020-03-06 14:48:16 +0100

    Added slight smoothing of PreviewLine, MSAA

--- PhrasingSpaceGameFinal/Assets/Scripts/PreviewLine.cs
+++ PhrasingSpaceGameFinal/Assets/Scripts/PreviewLine.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class PreviewLine : MonoBehaviour
 {
     [SerializeField] int segmentAmount;
+    [SerializeField] Color startColor;
+    [SerializeField] Color endColor;
 
     Vector2[] lastPositions;
+    Vector2[] currentPositions;
 
     GravityEffector[] effectors;
     PlayerMovement player;
@@ -17,8 +20,11 @@ public class PreviewLine : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startColor = startColor;
+        lineRenderer.endColor = endColor;
         lineRenderer.positionCount = segmentAmount;
         lastPositions = new Vector2[segmentAmount];
+        currentPositions = new Vector2[segmentAmount];
         for (int i = 0; i < segmentAmount; i++) lastPositions[i] = Vector2.zero;
         player = FindObjectOfType<PlayerMovement>();
         playerRB = player.GetComponent<Rigidbody2D>();
@@ -26,26 +32,45 @@ public class PreviewLine : MonoBehaviour
     }
 
 
-    void Update()
+    void FixedUpdate()
+    {
+        int breakIndex;
+        CalculatePositions(out breakIndex);
+        for(int i = 0; i < breakIndex; i++) lineRenderer.SetPosition(i, Vector3.Lerp(lastPositions[i], currentPositions[i], .8f));
+        if(breakIndex < segmentAmount) SetAllPositionsAfter(breakIndex, currentPositions[breakIndex]);
+        SetLastPositions();
+    }
+
+
+    void CalculatePositions(out int breakIndex) 
     {
         Vector3 position = player.transform.position;
         Vector3 velocity = playerRB.velocity;
 
-        for (int i = 0; i < segmentAmount; i++) 
+        for (int i = 0; i < segmentAmount; i++)
         {
-            lineRenderer.SetPosition(i, position);
             position += velocity * Time.fixedDeltaTime;
-            for(int j = 0; j < effectors.Length; j++) 
+            for (int j = 0; j < effectors.Length; j++)
             {
                 Vector2 gravity = effectors[j].GetGravityAtPoint(position);
                 velocity += new Vector3(gravity.x, gravity.y, .0f) * Time.fixedDeltaTime;
 
-                if(Vector3.Distance(effectors[j].transform.position, position) < effectors[j].Radius) 
+                if (Vector3.Distance(effectors[j].transform.position, position) < effectors[j].Radius)
                 {
-                    SetAllPositionsAfter(i, position);
+                    breakIndex = i;
                     return;
                 }
             }
+            currentPositions[i] = position;
+        }
+        breakIndex = segmentAmount;
+    }
+
+    void SetLastPositions() 
+    {
+        for(int i = 0; i < segmentAmount; i++) 
+        {
+            lastPositions[i] = currentPositions[i];
         }
     }
 
