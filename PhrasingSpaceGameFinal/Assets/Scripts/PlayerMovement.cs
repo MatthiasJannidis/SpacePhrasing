ef43d9f71be769da1f9617ebbe5f049d56435c4d
Clement-pirelli <clepirelli@gmail.com>
2020-03-11 11:41:20 +0100

    Implemented boost

--- PhrasingSpaceGameFinal/Assets/Scripts/PlayerMovement.cs
+++ PhrasingSpaceGameFinal/Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] UnityEvent onStopBoost = new UnityEvent();
     Rigidbody2D rb;
     bool boosting = false;
+    Timer boostRegenTimer = new Timer(.0f);
+    float boostFuel = .0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        boostFuel = settings.boostFuelAmount;
     }
 
     void FixedUpdate()
@@ -27,24 +30,32 @@ public class PlayerMovement : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(.0f, .0f, angle-90.0f);
 
-
-
-        //boost if clicked
-        if (Input.GetKey(settings.boostKey)) 
+        //boost if clicked and enough boost
+        if (Input.GetKey(settings.boostKey) && boostFuel > .001f) 
         {
-            if(boosting == false) 
+            if(boosting == false)
             {
                 boosting = true;
                 onStartBoost.Invoke();
             }
             Vector3 velocity = lookAt * settings.boostSpeed * Time.deltaTime;
             rb.velocity += new Vector2(velocity.x, velocity.y);
+            boostFuel -= Time.deltaTime * settings.boostFuelBurnSpeed;
         } else 
         {
             if(boosting == true) 
             {
                 boosting = false;
+                boostRegenTimer.reset(settings.boostRegenCooldown);
                 onStopBoost.Invoke();
+            
+            }
+            boostRegenTimer.tick();
+
+            if (boostRegenTimer.isDone) 
+            {
+                boostFuel += Time.deltaTime + settings.boostRegenSpeed;
+                boostFuel = Mathf.Clamp(boostFuel, .0f, settings.boostFuelAmount);
             }
         }
     }
